73e7a2f228d9ed461766e294ed2ba2c98d019c37
Ava Johnson <ava.johnson@zohomail.com>
2021-05-21 21:27:55 -0700

    Dialog managment

--- src/components/Dialog.svelte
+++ src/components/Dialog.svelte
@@ -4,19 +4,28 @@
   import { portal } from '../utils'
   const dispatch = createEventDispatcher()
 
+  let clazz = ''
+  export { clazz as class }
+
   onMount(() => {
     const main = document.getElementsByTagName('main')[0]
     main.classList.add('filter', 'blur')
-    return () => main.classList.remove('filter', 'blur')
+    history.pushState('dialog', null)
+    return () => {
+      main.classList.remove('filter', 'blur')
+      if (history.state === 'dialog') history.back()
+    }
   })
+
+  function close() {
+    dispatch('close')
+  }
 </script>
 
-<div
-  use:portal
-  class="fixed inset-0 z-50 flex overflow-auto items-center justify-center"
-  on:click={() => dispatch('close')}
->
-  <div on:click|stopPropagation transition:fly={{ duration: 300, y: -50 }} class="p-4 rounded bg-white shadow">
+<svelte:window on:keydown={e => e.key == 'Escape' && close()} on:popstate={close} />
+
+<div use:portal class="fixed inset-0 z-50 flex overflow-auto items-center justify-center" on:click={close}>
+  <div on:click|stopPropagation transition:fly={{ duration: 300, y: -50 }} class="{clazz} rounded bg-white shadow">
     <slot />
   </div>
 </div>
